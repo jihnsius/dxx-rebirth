eec7a99c3ead023a5024c2d0c1ba390425b7754f
Kp <kp@valhallalegends.com>
2012-06-17 05:08:12 +0000

    Add upstream-diff.sh to simplify exporting changes

---
+++ upstream-diff.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ -n "$1" ]]; then
+	tag="$1"; shift
+else
+	tag="$(git describe --tags --abbrev=0)"
+fi
+
+printf -v extra_version '%s%s%s' \
+	"$(git describe --tags 2>/dev/null)" \
+	"$(git diff --name-status --quiet --exit-code --cached || echo '+')" \
+	"$(git diff --name-status --quiet --exit-code || echo '*')"
+git diff "$tag.." | sed -e '/^\+extra_version =/s|'"''|'$extra_version'|"
