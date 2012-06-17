457e6b09b1646c7c7627d18eb831ce2380e01f1e
Kp <kp@valhallalegends.com>
2012-06-17 17:03:11 +0000

    console: mark private entries as static

--- main/console.c
+++ main/console.c
@@ -20,8 +20,8 @@
 #include "vers_id.h"
 #include "timer.h"
 
-PHYSFS_file *gamelog_fp=NULL;
-struct console_buffer con_buffer[CON_LINES_MAX];
+static PHYSFS_file *gamelog_fp=NULL;
+static struct console_buffer con_buffer[CON_LINES_MAX];
 static int con_state = CON_STATE_CLOSED, con_scroll_offset = 0, con_size = 0;
 extern void game_flush_inputs();
 
@@ -29,7 +29,7 @@ extern void game_flush_inputs();
 void cxx_handle_misc_con_key(const unsigned key);
 void cxx_con_interactive_print(int *const py);
 
-void con_add_buffer_line(int priority, char *buffer)
+static void con_add_buffer_line(int priority, char *buffer)
 {
 	int i=0;
 
@@ -109,7 +109,7 @@ void con_printf(int priority, const char *fmt, ...)
 	}
 }
 
-void con_draw(void)
+static void con_draw(void)
 {
 	int i = 0, y = 0, done = 0;
 
@@ -167,7 +167,7 @@ void con_draw(void)
 	gr_printf(SWIDTH-FSPACX(110),FSPACY(1),"PAGE-UP/DOWN TO SCROLL");
 }
 
-int con_handler(window *wind, d_event *event)
+static int con_handler(window *wind, d_event *event)
 {
 	int key;
 	static fix64 last_scroll_time = 0;
@@ -270,7 +270,7 @@ void con_showup(void)
 	}
 }
 
-void con_close(void)
+static void con_close(void)
 {
 	if (gamelog_fp)
 		PHYSFS_close(gamelog_fp);
