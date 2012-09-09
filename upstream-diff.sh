9d6fd8b126881328e648724846f43129fd262f95
Kp <kp@valhallalegends.com>
2012-09-09 19:30:02 +0000

    Exclude private tags from upstream-diff

--- upstream-diff.sh
+++ upstream-diff.sh
@@ -3,11 +3,11 @@
 if [[ -n "$1" ]]; then
 	tag="$1"; shift
 else
-	tag="$(git describe --tags --abbrev=0)"
+	tag="$(git describe --match=0\* --tags --abbrev=0)"
 fi
 
 printf -v extra_version '%s%s%s' \
-	"$(git describe --tags 2>/dev/null)" \
+	"$(git describe --match=0\* --tags 2>/dev/null)" \
 	"$(git diff --name-status --quiet --exit-code --cached || echo '+')" \
 	"$(git diff --name-status --quiet --exit-code || echo '*')"
 git diff --no-renames "$tag.." | sed -e '/^\+extra_version =/s|'"''|'$extra_version'|"
