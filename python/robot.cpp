293d44601f927f59259f3dc79d17de9a7ebb3295
Kp <kp@valhallalegends.com>
2012-12-08 20:07:00 +0000

    Define Python enum for builtin robot types

--- python/robot.cpp
+++ python/robot.cpp
@@ -1,13 +1,82 @@
 #include "filter.hpp"
 #include "wrap-filter-container.hpp"
 #include "wrap-object.hpp"
+#include "defmodules.hpp"
 #include "robot.hpp"
 
 using namespace boost::python;
 
 template <>
-void define_enum_values<robot_type_t>(enum_<robot_type_t>&)
+void define_enum_values<robot_type_t>(enum_<robot_type_t>& e)
 {
+	e
+		.value("hulk_concussion", hulk_concussion)
+		.value("medium_lifter", medium_lifter)
+		.value("spider_energy_ball", spider_energy_ball)
+		.value("cls1_drone", cls1_drone)
+		.value("medium_scout", medium_scout)
+		.value("cloaked_vulcan", cloaked_vulcan)
+		.value("cloaked_hulk_concussion", cloaked_hulk_concussion)
+		.value("supervisor1", supervisor1)
+		.value("screaming_laser", screaming_laser)
+		.value("plasma", plasma)
+		.value("supervisor2", supervisor2)
+		.value("platform_laser", platform_laser)
+		.value("platform_missile", platform_missile)
+		.value("triangle_useless", triangle_useless)
+		.value("mini", mini)
+		.value("hulk_fusion", hulk_fusion)
+		.value("hulk_homing", hulk_homing)
+		.value("d1_boss_lvl7", d1_boss_lvl7)
+		.value("cloaked_medium_lifter", cloaked_medium_lifter)
+		.value("vulcan", vulcan)
+		.value("hulk_green_laser", hulk_green_laser)
+		.value("spider_four_claw_melee", spider_four_claw_melee)
+		.value("multi_laser", multi_laser)
+		.value("d1_final_boss", d1_final_boss)
+		.value("smart_mine_energy_claws", smart_mine_energy_claws)
+		.value("phoenix_smelter", phoenix_smelter)
+		.value("spreadfire_spider", spreadfire_spider)
+		.value("gauss", gauss)
+		.value("spider_many_small_globes", spider_many_small_globes)
+		.value("spreadfire_spider_squad", spreadfire_spider_squad)
+		.value("smart_mine_energy_flash_missile", smart_mine_energy_flash_missile)
+		.value("d2_red_fatty_boss1", d2_red_fatty_boss1)
+		.value("d1_boss_lvl7_not_boss", d1_boss_lvl7_not_boss)
+		.value("guide", guide)
+		.value("plasma_body_but_homing_missile", plasma_body_but_homing_missile)
+		.value("kamakazie_guide_body", kamakazie_guide_body)
+		.value("vulcan_concussion_hybrid", vulcan_concussion_hybrid)
+		.value("pest", pest)
+		.value("internal_tactical_droid", internal_tactical_droid)
+		.value("pig", pig)
+		.value("diamond_claw", diamond_claw)
+		.value("snake_red", snake_red)
+		.value("thief", thief)
+		.value("mercury", mercury)
+		.value("ebandit", ebandit)
+		.value("d2_megamissile_phoenix_boss", d2_megamissile_phoenix_boss)
+		.value("d2_smartmine_mercury_boss", d2_smartmine_mercury_boss)
+		.value("boarhead", boarhead)
+		.value("missile_spider", missile_spider)
+		.value("kamakazie_fish", kamakazie_fish)
+		.value("mini_flash_missile", mini_flash_missile)
+		.value("lou_guard", lou_guard)
+		.value("d2_mercury_boss", d2_mercury_boss)
+		.value("yellow_fatty_miniboss_phoenix_mercury", yellow_fatty_miniboss_phoenix_mercury)
+		.value("cloaked_diamond_claw", cloaked_diamond_claw)
+		.value("cloaked_phoenix_smelter", cloaked_phoenix_smelter)
+		.value("unarmed_fish", unarmed_fish)
+		.value("phoenix_smelter2", phoenix_smelter2)
+		.value("kamakazie_fish2", kamakazie_fish2)
+		.value("smart_mine_energy_claws2", smart_mine_energy_claws2)
+		.value("missile_spider2", missile_spider2)
+		.value("snake_green", snake_green)
+		.value("d2_omega_homing_flash_boss", d2_omega_homing_flash_boss)
+		.value("snake_green2", snake_green2)
+		.value("d2_earthshaker_boss", d2_earthshaker_boss)
+		.value("reactor", reactor)
+		;
 }
 
 DEFINE_DXX_OBJECT_SUBTYPE_STATIC(robot);
--- python/robot.hpp
+++ python/robot.hpp
@@ -5,6 +5,72 @@
 
 enum robot_type_t
 {
+	hulk_concussion,
+	medium_lifter,	// "green guy"
+	spider_energy_ball,	// with mini-bots inside
+	cls1_drone,
+	medium_scout,	// blue, fast
+	cloaked_vulcan,
+	cloaked_hulk_concussion,
+	supervisor1,	// blue, no weapons
+	screaming_laser,	// one leg, right side
+	plasma,
+	supervisor2,	// white, laser
+	platform_laser,
+	platform_missile,
+	triangle_useless,	// no weapons, slow
+	mini,	// dropped by spider
+	hulk_fusion,
+	hulk_homing,
+	d1_boss_lvl7,
+	cloaked_medium_lifter,
+	vulcan,
+	hulk_green_laser,
+	spider_four_claw_melee,
+	multi_laser,
+	d1_final_boss,
+	smart_mine_energy_claws,
+	phoenix_smelter,
+	spreadfire_spider,
+	gauss,
+	spider_many_small_globes,
+	spreadfire_spider_squad,
+	smart_mine_energy_flash_missile,
+	d2_red_fatty_boss1,
+	d1_boss_lvl7_not_boss,
+	guide,
+	plasma_body_but_homing_missile,
+	kamakazie_guide_body,
+	vulcan_concussion_hybrid,
+	pest,
+	internal_tactical_droid,
+	pig,
+	diamond_claw,
+	snake_red,
+	thief,
+	mercury,
+	ebandit,
+	d2_megamissile_phoenix_boss,
+	d2_smartmine_mercury_boss,
+	boarhead,
+	missile_spider,
+	kamakazie_fish,
+	mini_flash_missile,
+	lou_guard,
+	d2_mercury_boss,
+	yellow_fatty_miniboss_phoenix_mercury,
+	cloaked_diamond_claw,
+	cloaked_phoenix_smelter,
+	unarmed_fish,
+	phoenix_smelter2,
+	kamakazie_fish2,
+	smart_mine_energy_claws2,
+	missile_spider2,
+	snake_green,
+	d2_omega_homing_flash_boss,
+	snake_green2,
+	d2_earthshaker_boss,
+	reactor,
 };
 
 DECLARE_DXX_OBJECT_TYPESAFE_SUBTYPE(robot);
