jihnsius/dxx-rebirth

commit e598155137d9f6d2e47fd8ece2fca7d6f9f5c80a

Fix additional build breaks and warnings.

main/render.cpp
@@ -351,6 +351,9 @@ static void check_face(segnum_t segnum, int sidenum, int facenum, int nv, int *v
 {
 	int	i;
 
+	tmap1;
+	tmp2;
+
 	if (_search_mode) {
 		int save_lighting;
 #ifndef OGL

arch/sdl/gr.cpp
@@ -132,7 +132,7 @@ int gr_set_mode(u_int32_t mode)
 	grd_curscreen->sc_w = w;
 	grd_curscreen->sc_h = h;
 	grd_curscreen->sc_aspect = fixdiv(grd_curscreen->sc_w*GameCfg.AspectX,grd_curscreen->sc_h*GameCfg.AspectY);
-	gr_init_canvas(&grd_curscreen->sc_canvas, canvas->pixels, BM_LINEAR, w, h);
+	gr_init_canvas(&grd_curscreen->sc_canvas, (unsigned char *)canvas->pixels, BM_LINEAR, w, h);
 	window_update_canvases();
 	gr_set_current_canvas(NULL);
 
@@ -186,11 +186,13 @@ int gr_init(int mode)
 	if (GameArg.SysNoBorders)
 		sdl_video_flags|=SDL_NOFRAME;
 
+#ifndef OGL
 	if (GameArg.DbgSdlHWSurface)
 		sdl_video_flags|=SDL_HWSURFACE;
 
 	if (GameArg.DbgSdlASyncBlit)
 		sdl_video_flags|=SDL_ASYNCBLIT;
+#endif
 
 	// Set the mode.
 	if ((retcode=gr_set_mode(mode)))

arch/win32/include/precomp.h
@@ -41,6 +41,12 @@
 #include <SDL/SDL_mixer.h>
 #include <digi_audio.h>
 
+#undef min
+#undef max
+
+using std::min;
+using std::max;
+
 typedef LONG_PTR ssize_t;
 
 #pragma warning(disable:4244) // warning C4244: '=': conversion from 'type1' to 'type2', possible loss of data

main/gamerend.cpp
@@ -510,7 +510,7 @@ void show_extra_views()
 			Guided_missile[Player_num] = NULL;
 		}
 
-		unsigned other_player_missile_player_idx = ~0;
+		unsigned other_player_missile_player_idx = (unsigned)~0;
 		if (!Missile_viewer || Missile_viewer->type != OBJ_WEAPON || Missile_viewer->ctype.laser_info.parent_num != Players[Player_num].objnum)
 		{
 			dxxobject *local_player_missile = NULL;
@@ -539,7 +539,7 @@ void show_extra_views()
 				}
 				if (other_player_missile)
 					continue;
-				unsigned iplr = 0;
+				int iplr = 0;
 				for (; iplr < N_players; ++iplr)
 				{
 					if (iplr == Player_num)
@@ -594,7 +594,7 @@ void show_extra_views()
 				callsign = "SHIP";
 			else
 			{
-				for (unsigned p = 0; p != N_players; ++p)
+				for (int p = 0; p != N_players; ++p)
 					if (Viewer == &Objects[Players[p].objnum])
 					{
 						callsign = Players[p].callsign;

main/gauges.cpp
@@ -2588,7 +2588,7 @@ static unsigned show_player_score_with_position(const unsigned player_num, const
 		 * If self, then rotate relative to special.
 		 * Else, rotate relative to object.
 		 */
-		if (Player_num == player_num)
+		if (Player_num == (int)player_num)
 			return 0;
 		g3s_point player_point;
 		g3_rotate_point(&player_point, vvop);

main/weapon.cpp
@@ -1141,7 +1141,7 @@ void DropSecondaryWeapon ()
 {
 	objnum_t objnum;
 	int seed;
-	ubyte weapon_drop_id=-1;
+	ubyte weapon_drop_id=(ubyte)-1;
 	ushort sub_ammo=0;
 
 	if (num_objects >= MAX_USED_OBJECTS)

misc/hmp.cpp
@@ -349,7 +349,7 @@ static int setup_buffers(hmp_file *hmp) {
 
 	lastbuf = NULL;
 	for (i = 0; i < HMP_BUFFERS; i++) {
-		if (!(buf = d_malloc(HMP_BUFSIZE + sizeof(MIDIHDR))))
+		if (!(buf = (MIDIHDR*)d_malloc(HMP_BUFSIZE + sizeof(MIDIHDR))))
 			return HMP_OUT_OF_MEM;
 		memset(buf, 0, sizeof(MIDIHDR));
 		buf->lpData = (char *)buf + sizeof(MIDIHDR);

commit 781701b38935c8018ca1079f6bc3206411276aab

Fix render break introduced in e598155137d9f6d
tmap2 cannot be abbreviated as tmp2.

main/render.cpp
@@ -347,12 +347,9 @@ static void render_face(segnum_t segnum, int sidenum, int nv, int *vp, int tmap1
 // ----------------------------------------------------------------------------
 //	Only called if editor active.
 //	Used to determine which face was clicked on.
-static void check_face(segnum_t segnum, int sidenum, int facenum, int nv, int *vp, int tmap1 __attribute_unused, int tmap2 __attribute_unused, uvl *uvlp)
+static void check_face(segnum_t segnum, int sidenum, int facenum, int nv, int *vp, int /* tmap1 */, int /* tmap2 */, uvl *uvlp)
 {
 	int	i;
-
-	tmap1;
-	tmp2;
 
 	if (_search_mode) {
 		int save_lighting;