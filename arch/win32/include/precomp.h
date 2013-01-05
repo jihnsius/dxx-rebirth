52325e89dd339c9e284b1fb726d3821c9a47fa92
Skywing <skywing@valhallalegends.com>
2013-01-05 14:35:21 -0800

    Flesh out the Win32 precompiled header some more.

--- arch/win32/include/precomp.h
+++ arch/win32/include/precomp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #define _WIN32_WINNT 0x0501 // for get/freeaddrinfo()
+#define _CRT_SECURE_NO_WARNINGS
+#define STRSAFE_NO_DEPRECATE
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -19,6 +21,14 @@
 #include <time.h>
 #include <ctype.h>
 
+#include <strsafe.h>
+
+#define vsnprintf StringCbVPrintfA
+#define snprintf StringCbPrintfA
+#define strdup _strdup
+#define stricmp _stricmp
+#define strcasecmp _stricmp
+
 #include <algorithm>
 #include <array>
 
@@ -26,5 +36,9 @@
 #include <SDL/SDL_audio.h>
 #include <SDL/SDL_mixer.h>
 #include <digi_audio.h>
+
+typedef LONG_PTR ssize_t;
+
+#pragma warning(disable:4244) // warning C4244: '=': conversion from 'type1' to 'type2', possible loss of data
 
 #pragma hdrstop
