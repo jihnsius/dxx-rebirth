jihnsius/dxx-rebirth

commit 03799d88ca9c22717afd8895a686d0f222e7efe1

Use typedef segnum_t for segment number

main/aistruct.h
@@ -143,7 +143,7 @@ extern "C" {
 typedef struct ai_static {
 	ubyte   behavior;               //
 	sbyte   flags[MAX_AI_FLAGS];    // various flags, meaning defined by constants
-	short   hide_segment;           // Segment to go to for hiding.
+	segnum_t   hide_segment;           // Segment to go to for hiding.
 	short   hide_index;             // Index in Path_seg_points
 	short   path_length;            // Length of hide path.
 	sbyte   cur_path_index;         // Current index in path.
@@ -151,13 +151,13 @@ typedef struct ai_static {
 	short   danger_laser_num;
 	int     danger_laser_signature;
 	fix64   dying_start_time;       // Time at which this robot started dying.
-} __pack__ ai_static;
+} ai_static;
 
 // Same as above but structure Savegames/Multiplayer objects expect
 typedef struct ai_static_rw {
 	ubyte   behavior;               //
 	sbyte   flags[MAX_AI_FLAGS];    // various flags, meaning defined by constants
-	short   hide_segment;           // Segment to go to for hiding.
+	segnum_t   hide_segment;           // Segment to go to for hiding.
 	short   hide_index;             // Index in Path_seg_points
 	short   path_length;            // Length of hide path.
 	sbyte   cur_path_index;         // Current index in path.
@@ -176,7 +176,7 @@ typedef struct ai_local {
 	int        mode;                          // current mode within behavior
 	int        previous_visibility;           // Visibility of player last time we checked.
 	int        rapidfire_count;               // number of shots fired rapidly
-	int        goal_segment;                  // goal segment for current path
+	segnum_t        goal_segment;                  // goal segment for current path
 	fix        next_action_time;              // time in seconds until something happens, mode dependent
 	fix        next_fire;                     // time in seconds until can fire again
 	fix        next_fire2;                    // time in seconds until can fire again from second weapon
@@ -200,7 +200,7 @@ typedef struct ai_local_rw {
 	int        mode;                          // current mode within behavior
 	int        previous_visibility;           // Visibility of player last time we checked.
 	int        rapidfire_count;               // number of shots fired rapidly
-	int        goal_segment;                  // goal segment for current path
+	segnum_t        goal_segment;                  // goal segment for current path
 	fix        next_action_time;              // time in seconds until something happens, mode dependent
 	fix        next_fire;                     // time in seconds until can fire again
 	fix        next_fire2;                    // time in seconds until can fire again from second weapon
@@ -217,24 +217,24 @@ typedef struct ai_local_rw {
 
 typedef struct {
 	fix64       last_time;
-	int         last_segment;
+	segnum_t         last_segment;
 	vms_vector  last_position;
 } ai_cloak_info;
 
 // Same as above but structure Savegames expect
 typedef struct {
 	fix         last_time;
-	int         last_segment;
+	segnum_t         last_segment;
 	vms_vector  last_position;
 } ai_cloak_info_rw;
 
 typedef struct {
-	int         segnum;
+	segnum_t         segnum;
 	vms_vector  point;
 } point_seg;
 
 typedef struct {
-	short       start, end;
+	segnum_t       start, end;
 } seg_seg;
 
 #define MAX_POINT_SEGS  2500

commit 165f45b80bc8ad5d6eb76e95807dbaf58568e7cd

Fix savegame regression from 03799d88ca9c22717afd8895a686d0f222e7efe1
Commit 03799d88ca9c22717afd8895a686d0f222e7efe1 converted from int to
segnum_t some fields that are copied to disk raw.  This changed the
format of savegames.  Worse, point_seg is saved via raw copy, but
reloaded piecewise, so savegames written using
03799d88ca9c22717afd8895a686d0f222e7efe1 or later cannot be reloaded
correctly.  This commit splits point_seg into point_seg and
point_seg_rw, and reverts the other incorrect changes.

main/ai.cpp
@@ -1566,7 +1566,13 @@ int ai_save_state(PHYSFS_file *fp)
 		PHYSFS_write(fp, ail_rw, sizeof(ai_local_rw), 1);
 		d_free(ail_rw);
 	}
-	PHYSFS_write(fp, Point_segs, sizeof(point_seg) * MAX_POINT_SEGS, 1);
+	for (i = 0; i < MAX_POINT_SEGS; ++i)
+	{
+		point_seg_rw seg_rw;
+		seg_rw.segnum = Point_segs[i].segnum;
+		seg_rw.point = Point_segs[i].point;
+		PHYSFS_write(fp, &seg_rw, sizeof(seg_rw), 1);
+	}
 	//PHYSFS_write(fp, Ai_cloak_info, sizeof(ai_cloak_info) * MAX_AI_CLOAK_INFO, 1);
 	for (i = 0; i < MAX_AI_CLOAK_INFO; i++)
 	{

main/aistruct.h
@@ -199,7 +199,7 @@ typedef struct ai_local_rw {
 	int        mode;                          // current mode within behavior
 	int        previous_visibility;           // Visibility of player last time we checked.
 	int        rapidfire_count;               // number of shots fired rapidly
-	segnum_t        goal_segment;                  // goal segment for current path
+	int        goal_segment;                  // goal segment for current path
 	fix        next_action_time;              // time in seconds until something happens, mode dependent
 	fix        next_fire;                     // time in seconds until can fire again
 	fix        next_fire2;                    // time in seconds until can fire again from second weapon
@@ -223,7 +223,7 @@ typedef struct {
 // Same as above but structure Savegames expect
 typedef struct {
 	fix         last_time;
-	segnum_t         last_segment;
+	int         last_segment;
 	vms_vector  last_position;
 } ai_cloak_info_rw;
 
@@ -231,6 +231,11 @@ typedef struct {
 	segnum_t         segnum;
 	vms_vector  point;
 } point_seg;
+
+typedef struct {
+	int segnum;
+	vms_vector point;
+} point_seg_rw;
 
 typedef struct {
 	segnum_t       start, end;